ee4d26130d1c19b38d127f4f7309561f43965021
positiveWand <49838556+positiveWand@users.noreply.github.com>
2021-12-17 17:38:13 +0900

    target 파일 업데이트, 사소한 수정

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class MainServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("Post Request received!!");
     }
 
     @Override
